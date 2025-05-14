439c13308a206f943172b1b257b5fe7b8b2956ac
mobius-15 <usn_cv16_gonavy@outlook.jp>
2025-05-14 19:20:00 +0900

    Update BaseDAO.java

--- project2025/src/main/java/dao/BaseDAO.java
+++ project2025/src/main/java/dao/BaseDAO.java
@@ -10,7 +10,7 @@ public class BaseDAO {
 	    private static final String JDBC_DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
 	    private static final String URL = "jdbc:mysql://localhost:3306/customersystem?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
 	    private static final String USER = "root";
-	    private static final String PASSWORD = "qq3g9yp";
+	    private static final String PASSWORD = "";
 	    protected Connection conn = null;
 	    
 	    public void open() throws ClassNotFoundException, SQLException {	       

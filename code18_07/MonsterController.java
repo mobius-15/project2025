f7303788c6f2ceff106e4d1e16f986b5b5c7f8aa
mobius-15 <usn_cv16_gonavy@outlook.jp>
2025-09-19 07:28:17 +0900

    Delete code18_07 directory

--- code18_07/MonsterController.java
+++
@@ -1,9 +0,0 @@
-package code18_07;
-import org.springframeeork.web.bind.annotation.*
-@RestController
-public class MonsterController {
-	@GetMapping("/monster")
-	public Monster monster(RequestParam(value="id")String id){
-		return new Monster(id);
-	}
-}
